21e9302e456b30f34d472b8767a06f9816cbe26a
anmol_dhama <anmol.dhama@rapidshyp.com>
2025-04-29 13:00:28 +0530

    changes in config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 function connectToDb(){
-   mongoose.connect(process.env.DB_CONNECT as string).then(()=>{
+   mongoose.connect(process.env.DB_CONNECT || "mongodb+srv://anmoldham:RCuC7Y5vWjw4oMvd@cluster0.eawxw0q.mongodb.net/expense_app" as string).then(()=>{
     console.log('Connected to db');
    }).catch((err)=>{
     console.log(err);
